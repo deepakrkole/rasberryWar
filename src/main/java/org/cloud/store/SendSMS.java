8a88e1f294b0ce4e5a5b16212cce2a31c3b6e760
Arpit JOSHI <arpitjoshi_177@yahoo.com>
2015-05-13 18:40:09 -0700

    Sms Code commited

---
+++ src/main/java/org/cloud/store/SendSMS.java
@@ -0,0 +1,96 @@
+package org.cloud.store;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.mail.MailMessage;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.MessageFactory;
+import com.twilio.sdk.resource.instance.Message;
+
+
+/**
+ * 
+ * @author Arpit
+ *
+ */
+public class SendSMS {
+	static TwilioRestClient client ;
+    /* Twilio REST API version */
+    public static final String ACCOUNTSID = "ACefaca9918bcad722354f504a729611df";
+    public static final String AUTHTOKEN = "db1b2b6edbba48af871b51bdb652448f";
+    
+    public void sendEmail(String UserName, String emailId,String alertmessage){
+		System.out.println("Sending email to "+emailId);
+		final String sender = "cloudrasberry@gmail.com";
+		final String password = "rasberry";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(sender, password);
+			}
+		  });
+ 
+		try {
+ 
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("cloudrasberry@gmail.com"));
+			message.setRecipients(javax.mail.Message.RecipientType.TO,
+				InternetAddress.parse(emailId));
+			message.setSubject("Thank You Note from CloudRasberry ");
+			alertmessage="Thank You for getting in touch with us. We will get back to you ASAP";
+			message.setText(alertmessage);
+ 
+			Transport.send(message);
+ 
+			//System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+    
+    public void sendSMS(String userNumber,String smsMessage){
+    	
+    	client=new TwilioRestClient(ACCOUNTSID, AUTHTOKEN);
+    	
+   	 
+        // Build a filter for the MessageList
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("Body", smsMessage));
+        params.add(new BasicNameValuePair("To", userNumber));
+        params.add(new BasicNameValuePair("From", "+14072701082"));
+     
+        MessageFactory messageFactory = client.getAccount().getMessageFactory();
+        try {
+			Message message = messageFactory.create(params);
+		} catch (TwilioRestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+}
